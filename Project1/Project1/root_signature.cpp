2b7fd3484b57966afec45b112ff46f2d8aebcb27
ibuki-27 <haru.terada78@gmail.com>
2025-12-03 10:19:42 +0900

    ルートシグネチャ完成

--- Project1/Project1/root_signature.cpp
+++ Project1/Project1/root_signature.cpp
@@ -17,4 +17,46 @@ RootSignature::~RootSignature()
 	rootSignatureDesc.NumStaticSamplers = 0;
 	rootSignatureDesc.pStaticSamplers = nullptr;
 	rootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+	ID3DBlob* signature{};
+	auto res = D3D12SerializeRootSignature(
+		&rootSignatureDesc,
+		D3D_ROOT_SIGNATURE_VERSION_1,
+		&signature,
+		nullptr);
+
+	bool success = SUCCEEDED(res);
+	if (!success) {
+		assert(false && "ルートシグネチャのシリアライズに失敗");
+
+	}
+	else {
+		res = device.get()->CreateRootSignature(
+			0,
+			signature->GetBufferPointer(),
+			signature->GetBufferSize(),
+			IID_PPV_ARGS(&rootSignature_));
+
+		success &= SUCCEEDED(res);
+		if (!success) {
+			assert(false && "ルートシグネチャの生成に失敗");
+		}
+	}
+
+	if (signature)
+	{
+		signature->Release();
+	}
+
+	return success;
+}
+
+[[nodiscard]] ID3D12RootSignature* RootSignature::get() const noexcept {
+	if (!rootSignature_)
+	{
+		assert(false && "ルートシグネチャが生成されていません");
+
+	}
+	return rootSignature_;
+
 }
